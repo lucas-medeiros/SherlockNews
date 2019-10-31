9cd48d426a332fe8d4140d7ba5ec9c47daf4bc93
lucas-medeiros <lucascarmed@gmail.com>
2019-10-30 21:36:35 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,37 @@
-console.log("Hello World");
-
 paragraphs = document.getElementsByTagName('a');
 
-for (elt of paragraphs){
-    elt.style['background-color'] = '#FF00FF';
+var confiaveis = ['.globo.','.estadao.','glo.bo','.e-farsas.','.jusbrasil.','folha.']
+var naoconfiaveis = ['.oantagonista.','.sensacionalista.','.fake.','.ceticismopolitico.','.correiodopoder.','.criticapolitica.']
+var i=0
+var j=0
+var conflen = confiaveis.length;
+var unconflen = naoconfiaveis.length;
+
+setInterval(oneSecondFunction, 1000);
+	
+	
+function oneSecondFunction() {
+        
+    var arr = [], l = document.links;
+
+    for(var i=0; i<l.length; i++) {
+        arr.push(l[i].href);
+    
+        //l[i].style['background-color'] = '#FF00FF';
+    }
+    var k=0;
+    for (k=0;k<arr.length; k++){
+        for (i =0 ; i<conflen;i++){ 
+              if (arr[k].indexOf(confiaveis[i]) !== -1){
+                  l[k].style['background-color'] = 'rgba(0,255,0,0.5)';
+              }
+         }
+         i=0
+         for (i =0 ; i<unconflen;i++){
+             if (arr[k].indexOf(naoconfiaveis[i]) !== -1){
+                 l[k].style['background-color'] = 'rgba(255,0,0,0.5)';
+
+              }
+         }
+     }
 }
